c9323694de8cfef843f4e497182726c675a22b05
Maraza25 <agirmanberat668@gmail.com>
2024-03-29 22:45:51 +0300

    style: code organized according to Prettier

--- src/auth.ts
+++ src/auth.ts
@@ -1,8 +1,8 @@
 // src/auth.ts
 export function login(username: string, password: string): string {
-  if (username === "op_admin" && password === "opopopop") {
-    return "The user has successfully logged in.";
+  if (username === 'op_admin' && password === 'opopopop') {
+    return 'The user has successfully logged in.';
   } else {
-    return "The username or password is incorrect.";
+    return 'The username or password is incorrect.';
   }
 }
