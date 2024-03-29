9feea6fd4285a76ea009e2cc46023b1d1818068b
Maraza25 <agirmanberat668@gmail.com>
2024-03-29 03:52:12 +0300

    feat: prettier added

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,32 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended', // Bu satırı ekleyin
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+    'prettier', // Bu satırı ekleyin
+  ],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        // Bu bölümü ekleyin
+        arrowParens: 'always',
+        singleQuote: true,
+        bracketSpacing: false,
+        trailingComma: 'all',
+      },
     ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
-}
+  },
+};
